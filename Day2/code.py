fb743578116c7a30a8555eedb9bb36b04ff3e073
Rajat Kumar <grajatkumar54321@gmail.com>
2026-07-04 14:15:21 +0530

    ..

--- Day2/code.py
+++ Day2/code.py
@@ -2,13 +2,35 @@
 # print("hello\nworld", "with python")
 
 # variables and data types
-name = "rajat";
-age = 23;
-print("name is :", name, "\nage is : ", age)
-print(type(name))
+# follow snake case
+# name = "rajat";
+# age = 23;
+# print("name is :", name, "\nage is : ", age)
+# print(type(name))
 
-is_prime=True
-print(type(is_prime))
+# is_prime=True
+# print(type(is_prime))
 
-is_odd=None
-print(type(is_odd))
+# is_odd=None
+# print(type(is_odd))
+
+# wap to add two no's
+
+# num1=5
+# num2=9
+
+# sum= num1 + num2
+# print(sum)
+
+# print(not(num1 > num2)) #reverse the value
+
+# print((num1 < num2) and (10>4)) # true when both conditions are true
+# print((num1 < num2) or (10 < 4))
+
+# input from user
+
+a= int(input("enter value of a : "))
+b=int (input("enter value of b : "))
+
+add=a+b
+print(add)
